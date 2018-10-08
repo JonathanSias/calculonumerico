eb778e60dda96f859706406b69342f35e66e935d
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2018-10-08 12:58:32 -0300

    done

--- imp1.c
+++ imp1.c
@@ -25,13 +25,93 @@
 // [1, 2]               | 3
 // iteraçoes = 28       |
 //
-void falsaPosicao(float a, float b, int p){
-    // criterios de parada
-    // 1 - f(xk) < E
-    // 2 - |xk - xk-1| < E
-    // 3 - numero limite de iteraçoes
-    //float k0 = a;
-    //float k1 = b;
+void falsaPosicao(){
+    // 1
+    float a = 0;
+    float b = 1;
+    float fa;
+    float fb;
+    float x;
+    float fx;
+    int i = 0;
+    printf("\n\nEXERCÍCIO 1:\n");
+    while (i <= 3) {
+        fa = ((pow(a, 3))-(9*a)+3);
+        fb = ((pow(b, 3))-(9*b)+3);
+        x = ((a*fb)-(b*fa))/(fb-fa);
+        fx = ((pow(x, 3))-(9*x)+3);
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, x, fa, fb, fx);
+        if (fa >= 0 && fx >= 0) {
+            a = x;
+        }else{
+            if (fb >= 0 && fx >= 0) {
+                b = x;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = x;
+                }else{
+                    b = x;
+                }
+            }
+        }
+        i++;
+    }
+    // 2
+    a = 0;
+    b = 1;
+    fa = 0;
+    fb = 0;
+    i = 0;
+    printf("\n\nEXERCÍCIO 2:\n");
+    while (i <= 17) {
+        fa = ((exp(a))-(3*a));
+        fb = ((exp(b))-(3*b));
+        x = ((a*fb)-(b*fa))/(fb-fa);
+        fx = ((exp(x))-(3*x));
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, x, fa, fb, fx);
+        if (fa >= 0 && fx >= 0) {
+            a = x;
+        }else{
+            if (fb >= 0 && fx >= 0) {
+                b = x;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = x;
+                }else{
+                    b = x;
+                }
+            }
+        }
+        i++;
+    }
+    // 3
+    a = 1;
+    b = 2;
+    fa = 0;
+    fb = 0;
+    i = 0;
+    printf("\n\nEXERCÍCIO 3:\n");
+    while (i <= 28) {
+        fa = ((exp(a))-(3*a));
+        fb = ((exp(b))-(3*b));
+        x = ((a*fb)-(b*fa))/(fb-fa);
+        fx = ((exp(x))-(3*x));
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, x, fa, fb, fx);
+        if (fa >= 0 && fx >= 0) {
+            a = x;
+        }else{
+            if (fb >= 0 && fx >= 0) {
+                b = x;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = x;
+                }else{
+                    b = x;
+                }
+            }
+        }
+        i++;
+    }
 }
 
 // BISSECÇAO
@@ -48,94 +128,102 @@ void falsaPosicao(float a, float b, int p){
 // iteracoes = 18       |
 void bisseccao(){
     float pontoMedio;
-    int i = 1;
+    int i = 0;
     float fx;
     float fa;
     float fb;
-    float aux;
+    float a;
+    float b;
     // 1
-    float a = 0.5;
-    float b = 1;
+    a = 0.5;
+    b = 1;
     printf("\n\nEXERCÍCIO 1:\n");
     while (i <= 5) {   // criterios de parada
         fa = ((pow(a, 3))-(9*a)+5);
         fb = ((pow(b, 3))-(9*b)+5);
         pontoMedio = ((a+b)/2);
-        aux = pow(pontoMedio, 3);
-        fx = ((aux)-(9*pontoMedio)+5);
-        /*if ((fx == 0) || (pontoMedio < p)) {
-            // retorna pontoMedio
-        }*/
-        printf("%d a: %f, b: %f, x: %f\n", i, a, b, pontoMedio);
+        fx = ((pow(pontoMedio, 3))-(9*pontoMedio)+5);;
+        if ((fx == 0) || (pontoMedio < 0.01)) {
+            printf("SOLUÇÃO ENCONTRADA: %f\n", fx);
+            exit(1);
+        }
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, pontoMedio, fa, fb, fx);
         // testa se os sinais de "a" e do pontoMedio sao iguais
         // caso nao seja, testa se os sinais de "b" e do pontoMedio sao iguais
-        if (fx < 0) {
-            b = pontoMedio;
-        }else{
+        if (fa >= 0 && fx >= 0) {
             a = pontoMedio;
+        }else{
+            if (fb >= 0 && fx >= 0) {
+                b = pontoMedio;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = pontoMedio;
+                }else{
+                    b = pontoMedio;
+                }
+            }
         }
         i++;
     }
-    printf("\n\nEXERCÍCIO 2:\n");
-    ////////////////////////////////////////////////////////////////////////////
-    aux = 0;
-    i = 1;
+    // 2
+    i = 0;
     a = 0;
     b = 1;
-    // 2
-    while (i <= 30) {   // até chegar as 30 iterações
+    printf("\n\nEXERCÍCIO 2:\n");
+    while (i <= 30) {   // criterios de parada
         fa = ((exp(a))-(3*a));
         fb = ((exp(b))-(3*b));
         pontoMedio = ((a+b)/2);
-        aux = exp(pontoMedio);
-        fx = ((aux)-(3*pontoMedio));
-        /*if ((fx == 0) || (pontoMedio < p)) {
-            // retorna pontoMedio
-        }*/
-        printf("%d a: %f, b: %f, x: %f\n", i, a, b, pontoMedio);
+        fx = ((exp(pontoMedio))-(3*pontoMedio));
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, pontoMedio, fa, fb, fx);
         // testa se os sinais de "a" e do pontoMedio sao iguais
         // caso nao seja, testa se os sinais de "b" e do pontoMedio sao iguais
-        if (fx < 0) {
-            b = pontoMedio;
-        }else{
+        if (fa >= 0 && fx >= 0) {
             a = pontoMedio;
+        }else{
+            if (fb >= 0 && fx >= 0) {
+                b = pontoMedio;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = pontoMedio;
+                }else{
+                    b = pontoMedio;
+                }
+            }
         }
         i++;
     }
-    printf("\n\nEXERCÍCIO 3:\n");
-    ////////////////////////////////////////////////////////////////////////////
-    aux = 0;
-    i = 1;
+    // 3
+    i = 0;
     a = 1;
     b = 2;
-    //3
-    while (i <= 18) {   // até chegar as 18 iterações
-        fa = ((exp(a))-(3-a));
-        fb = ((exp(b))-(3-b));
-        pontoMedio = pontoMedio = ((a+b)/2);
-        aux = exp(pontoMedio);
-        fx = ((aux)-(3*pontoMedio));
-        /*if ((pontoMedio == 0) || (pontoMedio < p)) {
-            //printf("%f\n", pontoMedio);
-        }*/
-        //i++;
-        printf("%d a: %f, b: %f, x: %f\n", i, a, b, pontoMedio);
+    printf("\n\nEXERCÍCIO 3:\n");
+    while (i <= 18) {   // criterios de parada
+        fa = ((exp(a))-(3*a));
+        fb = ((exp(b))-(3*b));
+        pontoMedio = ((a+b)/2);
+        fx = ((exp(pontoMedio))-(3*pontoMedio));
+        printf("%d a: %.8f, b: %.8f, x: %.8f, fa: %.8f, fb: %.8f, fx: %.8f\n", i, a, b, pontoMedio, fa, fb, fx);
         // testa se os sinais de "a" e do pontoMedio sao iguais
         // caso nao seja, testa se os sinais de "b" e do pontoMedio sao iguais
-        if (fx < 0) {
+        if (fa >= 0 && fx >= 0) {
             a = pontoMedio;
         }else{
-            b = pontoMedio;
+            if (fb >= 0 && fx >= 0) {
+                b = pontoMedio;
+            }else{
+                if (fa < 0 && fx < 0) {
+                    a = pontoMedio;
+                }else{
+                    b = pontoMedio;
+                }
+            }
         }
         i++;
     }
 }
 
 int main(){
-    float x0;   // primeiro ponto do intervalo
-    float x1;   // segundo ponto do intervalo
-    int precisao;
-    int iteracoes;
     int flag = 1;
     int metodo;
     while (flag == 1) {
@@ -145,7 +233,7 @@ int main(){
         scanf("%d", &metodo);
         switch (metodo) {
             case 1:
-                //falsaPosicao();
+                falsaPosicao();
             break;
             case 2:
                 bisseccao();
